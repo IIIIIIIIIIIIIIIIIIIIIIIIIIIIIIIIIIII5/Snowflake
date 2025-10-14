911f4b2f688ddd0ac9b88860ef649a6fdb33ecc4
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-14 07:43:11 -0700

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -5,10 +5,13 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('profile')
     .setDescription('View your training stats')
-    .addUserOption(opt => opt.setName('user').setDescription('The Discord user to view').setRequired(false)),
+    .addUserOption(opt =>
+      opt.setName('user').setDescription('The Discord user to view').setRequired(false)
+    ),
 
   async execute(interaction) {
     await interaction.deferReply({ ephemeral: false });
+
     const db = await GetJsonBin();
     const target = interaction.options.getUser('user') || interaction.user;
     const trainings = db.Trainings?.[target.id] || { hosted: {}, cohosted: {}, supervised: {} };
@@ -16,7 +19,10 @@ module.exports = {
 
     const getStats = type => {
       const data = trainings[type] || {};
-      if (data.lastMonth !== monthKey) { data[monthKey] = 0; data.lastMonth = monthKey; }
+      if (data.lastMonth !== monthKey) {
+        data[monthKey] = 0;
+        data.lastMonth = monthKey;
+      }
       return { monthly: data[monthKey] || 0, total: data.total || 0 };
     };
 
@@ -25,17 +31,21 @@ module.exports = {
     const supervised = getStats('supervised');
 
     const robloxId = db.VerifiedUsers?.[target.id];
-    let username = 'Not Verified', url;
+    let username = 'Not Verified';
+    let url, avatarUrl;
+
     if (robloxId) {
       const info = await GetRobloxUserInfo(robloxId);
       username = info.name;
       url = `https://www.roblox.com/users/${robloxId}/profile`;
+      avatarUrl = `https://www.roblox.com/headshot-thumbnail/image?userId=${robloxId}&width=420&height=420&format=png`;
     }
 
     const embed = new EmbedBuilder()
       .setTitle(username)
       .setURL(url)
       .setColor(0x1abc9c)
+      .setThumbnail(avatarUrl || target.displayAvatarURL({ size: 128 }))
       .addFields(
         { name: 'Trainings Hosted This Month', value: `${hosted.monthly}`, inline: true },
         { name: 'Trainings Co-Hosted This Month', value: `${cohosted.monthly}`, inline: true },
