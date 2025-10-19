88324faf4267a474f750d2294fa6ab4b91329fcd
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-19 00:14:55 -0700

    Update roblox.js

--- roblox.js
+++ roblox.js
@@ -1,10 +1,12 @@
 const axios = require('axios');
 const admin = require('firebase-admin');
 const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+firebaseConfig.private_key = firebaseConfig.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(firebaseConfig)
 });
+
 
 const db = admin.firestore();
 
