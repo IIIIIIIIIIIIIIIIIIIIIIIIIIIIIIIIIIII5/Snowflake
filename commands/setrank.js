157cc10359ec9457b3be8847191b03338c630e4e
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-28 06:35:11 -0400

    Update setrank.js

--- commands/setrank.js
+++ commands/setrank.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { GetJsonBin, SetRank, GetRobloxUserId } = require('../roblox');
+const { GetJsonBin, GetRobloxUserId, SetRank } = require('../roblox');
+
+const ALLOWED_ROLE = '1423332095001890908';
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -9,10 +11,14 @@ module.exports = {
     .addStringOption(opt => opt.setName('rankname').setDescription('Rank name').setRequired(true)),
 
   async execute(interaction) {
+    if (!interaction.member.roles.cache.has(ALLOWED_ROLE))
+      return interaction.reply({ content: "You don't have permission to use this command.", ephemeral: true });
+
     await interaction.deferReply({ ephemeral: true });
     const db = await GetJsonBin();
     const guildId = interaction.guild.id;
-    if (!db.ServerConfig?.[guildId]?.GroupId) return interaction.editReply({ content: 'Group ID not set. Run /config first.' });
+    if (!db.ServerConfig?.[guildId]?.GroupId)
+      return interaction.editReply({ content: 'Group ID not set. Run /config first.' });
 
     const groupId = db.ServerConfig[guildId].GroupId;
     const username = interaction.options.getString('username');
