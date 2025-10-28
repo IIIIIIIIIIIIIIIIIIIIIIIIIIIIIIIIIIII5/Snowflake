387e04d2cb5b4521b0ee85d6c88eb33758da5054
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-28 06:10:39 -0400

    Create suspend.js

---
+++ commands/suspend.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { GetJsonBin, SetRank, GetRobloxUserId } = require('../roblox');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('suspend')
+    .setDescription("Suspend a user")
+    .addStringOption(opt => opt.setName('username').setDescription('Roblox username to suspend').setRequired(true)),
+
+    async execute(interaction) {
+      await interaction.deferReply({ ephemeral: true });
+      const db = await GetJsonBin();
+      const GuildId = interaction.guild.id;
+      if (!db.ServerConfig?.[GuildId]?.GroupId) return interaction.editReply({ content: 'Group ID not set. Run /config first.' });
+      
+      const GroupId = db.ServerConfig[GuildId].GroupId;
+      const username = interaction.options.getString('username');
+      const UserId = await GetRobloxUserId(username);
+      
+      try {
+        await SetRank(GroupId, UserId, "Suspended", interaction.user.id, GuildId);
+        return interaction.editReply({ content: `Suspended ${username}` });
+      } catch (err) {
+        return interaction.editReply({ content: `Error: ${err.message}` });
+      }
+    }
+};
