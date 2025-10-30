241975a9791f3f2a2b0344a080c7110bee9e4416
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-30 10:13:16 -0400

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const crypto = require('crypto');
-const { GetRobloxUserId, startVerification } = require('../roblox');
+const { GetRobloxUserId, startVerification, isUserVerified } = require('../roblox');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -10,13 +10,24 @@ module.exports = {
 
   async execute(interaction) {
     await interaction.deferReply({ ephemeral: true });
+
+    const alreadyVerified = await isUserVerified(interaction.user.id);
+    if (alreadyVerified) {
+      return interaction.editReply(
+        "You're already verified. If you want to switch your account, use `/reverify`."
+      );
+    }
+
     const username = interaction.options.getString('username');
     const userId = await GetRobloxUserId(username);
-    const code = 'VERIFY-' + crypto.randomBytes(3).toString('hex').toUpperCase();
 
+    const code = 'VERIFY-' + crypto.randomBytes(3).toString('hex').toUpperCase();
     startVerification(interaction.user.id, userId, code);
 
-    const button = new ButtonBuilder().setCustomId('done_verification').setLabel('Done').setStyle(ButtonStyle.Primary);
+    const button = new ButtonBuilder()
+      .setCustomId('done_verification')
+      .setLabel('Done')
+      .setStyle(ButtonStyle.Primary);
     const row = new ActionRowBuilder().addComponents(button);
 
     return interaction.editReply({
