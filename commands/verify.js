22d33daa81a08b5f5c4fc22ff39a36b6f0205ac2
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-30 10:36:29 -0400

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -1,43 +1,15 @@
 const { SlashCommandBuilder } = require('discord.js');
-const crypto = require('crypto');
-const { GetRobloxUserId, startVerification, isUserVerified } = require('../roblox');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('verify')
-    .setDescription('Verify your Roblox account')
-    .addStringOption(opt => opt.setName('username').setDescription('Your Roblox username').setRequired(true)),
+    .setDescription('Verify your Roblox account'),
 
   async execute(interaction) {
     await interaction.deferReply({ ephemeral: true });
-
-    const alreadyVerified = await isUserVerified(interaction.user.id);
-    if (alreadyVerified) {
-      return interaction.editReply(
-        "You're already verified. If you want to switch your account, use /reverify."
-      );
-    }
-
-    const username = interaction.options.getString('username');
-    const userId = await GetRobloxUserId(username);
-
-    const code = 'VERIFY-' + crypto.randomBytes(3).toString('hex').toUpperCase();
-    startVerification(interaction.user.id, userId, code);
-
-    try {
-      await interaction.user.send(
-        `Join the Roblox game and enter the verification code displayed below:\nYour verification code: ${code}`
-      );
-
-      return interaction.editReply({
-        content: 'I have sent you a DM with instructions to verify your Roblox account.',
-        ephemeral: true
-      });
-    } catch (err) {
-      return interaction.editReply({
-        content: 'I could not DM you. Please make sure your DMs are open and try again.',
-        ephemeral: true
-      });
-    }
+    return interaction.editReply({
+      content: "Join the Roblox game. The game will generate a unique code for you. Once you see it, enter it in Discord to complete verification.",
+      ephemeral: true
+    });
   }
 };
