5189a9f1e89ff03efe30d90964cda642e53e5afa
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-10-30 10:13:39 -0400

    Create reverify.js

---
+++ commands/reverify.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const crypto = require('crypto');
+const { GetRobloxUserId, startVerification, GetJsonBin, SaveJsonBin } = require('../roblox');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('reverify')
+    .setDescription('Switch your linked Roblox account')
+    .addStringOption(opt => opt.setName('username').setDescription('Your new Roblox username').setRequired(true)),
+
+  async execute(interaction) {
+    await interaction.deferReply({ ephemeral: true });
+
+    const username = interaction.options.getString('username');
+    const userId = await GetRobloxUserId(username);
+
+    const dbData = await GetJsonBin();
+    dbData.VerifiedUsers = dbData.VerifiedUsers || {};
+    if (dbData.VerifiedUsers[interaction.user.id]) {
+      delete dbData.VerifiedUsers[interaction.user.id];
+      await SaveJsonBin(dbData);
+    }
+
+    const code = 'VERIFY-' + crypto.randomBytes(3).toString('hex').toUpperCase();
+    startVerification(interaction.user.id, userId, code);
+
+    const button = new ButtonBuilder()
+      .setCustomId('done_verification')
+      .setLabel('Done')
+      .setStyle(ButtonStyle.Primary);
+    const row = new ActionRowBuilder().addComponents(button);
+
+    return interaction.editReply({
+      content: `Switching verification! Put this code in your Roblox profile description:\n${code}\nThen click the Done button when finished.`,
+      components: [row]
+    });
+  }
+};
