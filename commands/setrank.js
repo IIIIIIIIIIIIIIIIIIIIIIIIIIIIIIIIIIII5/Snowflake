c3ffe3d3b27078e7a50303488a61210dd2464ea2
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-11-07 10:22:39 -0500

    Update setrank.js

--- commands/setrank.js
+++ commands/setrank.js
@@ -36,7 +36,7 @@ module.exports = {
 
       const filtered = allRoles
         .filter(r => r.Name.toLowerCase().includes(focused))
-        .slice(0, 27);
+        .slice(0, 25);
 
       return interaction.respond(
         filtered.map(r => ({
