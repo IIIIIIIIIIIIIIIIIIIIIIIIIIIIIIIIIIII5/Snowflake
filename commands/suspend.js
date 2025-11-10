f0b18a187ed90abf4eef784af5777a48ea076722
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-11-10 10:29:19 -0500

    Update suspend.js

--- commands/suspend.js
+++ commands/suspend.js
@@ -109,7 +109,7 @@ module.exports = {
                 Interaction.user.id,
                 GuildId,
                 Interaction.client,
-                DurationMs,
+                DurationKey,
                 Reason
             );
 
