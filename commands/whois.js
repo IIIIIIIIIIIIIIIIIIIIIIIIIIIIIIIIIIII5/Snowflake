0f32b115ffa75f9a589344fc0b15cb09a0ecdeb8
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-12-09 11:17:56 -0500

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -1,44 +1,64 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { GetJsonBin, GetRobloxUserInfo } = require('../roblox');
-
-async function safeCall(promise, ms = 8000) {
-  return Promise.race([
-    promise,
-    new Promise((_, reject) => setTimeout(() => reject(new Error('Request timed out')), ms))
-  ]);
-}
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('whois')
     .setDescription('Lookup a Roblox user from a Discord user')
-    .addUserOption(opt => opt.setName('user').setDescription('The Discord user to look up').setRequired(false)),
+    .addUserOption(opt =>
+      opt.setName('user')
+        .setDescription('The Discord user to look up')
+        .setRequired(false)
+    ),
 
   async execute(interaction) {
     await interaction.deferReply({ ephemeral: true });
 
     let db;
     try {
-      db = await safeCall(GetJsonBin());
+      db = await GetJsonBin();
     } catch (err) {
-      return interaction.editReply({ content: `Failed to load verification database: ${err.message}` });
+      return interaction.editReply({
+        content: `Failed to load verification database: ${err.message}`
+      });
     }
 
     const target = interaction.options.getUser('user') || interaction.user;
     const robloxId = db.VerifiedUsers?.[target.id];
 
-    if (!robloxId)
-      return interaction.editReply({ content: `${target.tag} has not verified a Roblox account.` });
+    if (!robloxId) {
+      return interaction.editReply({
+        content: `${target.tag} has not verified a Roblox account.`
+      });
+    }
 
     let info;
     try {
-      info = await safeCall(GetRobloxUserInfo(robloxId));
+      info = await GetRobloxUserInfo(robloxId);
     } catch (err) {
-      return interaction.editReply({ content: `Failed to contact Roblox API: ${err.message}` });
+      return interaction.editReply({
+        content: `Failed to contact Roblox API: ${err.message}`
+      });
     }
 
-    return interaction.editReply({
-      content: `[${info.name}](https://www.roblox.com/users/${robloxId}/profile)`
-    });
+    const embed = new EmbedBuilder()
+      .setTitle(`${info.displayName} (${info.username})`)
+      .setURL(`https://www.roblox.com/users/${robloxId}/profile`)
+      .setThumbnail(info.avatar)
+      .setColor(0x2b2d31)
+      .addFields(
+        { name: "User ID", value: `${info.id}`, inline: true },
+        { name: "Created", value: info.created || "Unknown", inline: true },
+        { name: "Banned?", value: info.isBanned ? "Yes" : "No", inline: true },
+
+        { name: "Friends", value: `${info.friendsCount}`, inline: false },
+        { name: "Followers", value: `${info.followersCount}`, inline: true },
+        { name: "Following", value: `${info.followingCount}`, inline: true },
+
+        { name: "Description", value: info.description.length > 1024 ? info.description.slice(0, 1021) + "..." : info.description }
+      )
+      .setTimestamp();
+
+    return interaction.editReply({ embeds: [embed] });
   }
 };
