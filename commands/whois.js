2cde6604c31b838a95d5909e4b91ebeef0d0f9e6
IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII5 <jetpacked3@gmail.com>
2025-12-09 10:51:18 -0500

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -1,6 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { GetJsonBin, GetRobloxUserInfo } = require('../roblox');
 
+async function safeCall(promise, ms = 8000) {
+  return Promise.race([
+    promise,
+    new Promise((_, reject) => setTimeout(() => reject(new Error('Request timed out')), ms))
+  ]);
+}
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('whois')
@@ -9,12 +16,29 @@ module.exports = {
 
   async execute(interaction) {
     await interaction.deferReply({ ephemeral: true });
-    const db = await GetJsonBin();
+
+    let db;
+    try {
+      db = await safeCall(GetJsonBin());
+    } catch (err) {
+      return interaction.editReply({ content: `Failed to load verification database: ${err.message}` });
+    }
+
     const target = interaction.options.getUser('user') || interaction.user;
     const robloxId = db.VerifiedUsers?.[target.id];
-    if (!robloxId) return interaction.editReply({ content: `${target.tag} has not verified a Roblox account.` });
 
-    const info = await GetRobloxUserInfo(robloxId);
-    return interaction.editReply({ content: `[${info.name}](https://www.roblox.com/users/${robloxId}/profile)` });
+    if (!robloxId)
+      return interaction.editReply({ content: `${target.tag} has not verified a Roblox account.` });
+
+    let info;
+    try {
+      info = await safeCall(GetRobloxUserInfo(robloxId));
+    } catch (err) {
+      return interaction.editReply({ content: `Failed to contact Roblox API: ${err.message}` });
+    }
+
+    return interaction.editReply({
+      content: `[${info.name}](https://www.roblox.com/users/${robloxId}/profile)`
+    });
   }
 };
